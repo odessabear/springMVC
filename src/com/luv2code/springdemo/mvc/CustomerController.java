001dbc940e3520b4f579ba9bfe354762b0d124e2
odessabear <bearodessa@gmail.com>
2019-10-24 18:55:50 +0300

    Chapters 15 and 16 finished

--- src/com/luv2code/springdemo/mvc/CustomerController.java
+++ src/com/luv2code/springdemo/mvc/CustomerController.java
@@ -2,15 +2,31 @@ package com.luv2code.springdemo.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	
+	//add an IntiBinder ...to convert trim input strings
+	//remove leading and trailing whitespaces
+	//resolve issue for our validation
+	
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		
+		dataBinder.registerCustomEditor(String.class,stringTrimmerEditor);
+	}
+	
 	
 	@RequestMapping("/showForm")
 	public String showForm(Model theModel) {
@@ -23,6 +39,12 @@ public class CustomerController {
 	@RequestMapping("/processForm")
 	public String processform(
 		@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingresult) {
+		
+		System.out.println("Last name: |" + theCustomer.getLastName() + "|");
+		
+		System.out.println("Binding result: " + theBindingresult);
+		
+		System.out.println("\n\n\n\n");
 		
 		if(theBindingresult.hasErrors()) {
 			return "customer-form";
